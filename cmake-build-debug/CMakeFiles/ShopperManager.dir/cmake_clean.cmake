7bc5488ed5e2dbd1093da9ba22f208426c8b3c36
Felix David Garcia <felix.david21@outlook.com>
2017-10-18 17:14:21 -0700

    Added necesary files

---
+++ cmake-build-debug/CMakeFiles/ShopperManager.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShopperManager.dir/main.cpp.obj"
+  "ShopperManager.pdb"
+  "ShopperManager.exe"
+  "ShopperManager.exe.manifest"
+  "libShopperManager.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShopperManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
